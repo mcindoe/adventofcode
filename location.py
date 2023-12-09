8ad42797249bf781ae02ad2e38fd0a8fbabf4f1d
Conor McIndoe <conormcindoe1@gmail.com>
2023-12-09 09:50:00 +0000

    Work on utility functions

---
+++ location.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+YEAR_2023_DIR = ROOT_DIR / "year2023"
---
+++ year2023/day01/src/utils.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+from adventofcode.location import YEAR_2023_DIR
+
+
+def get_data_file(url: str, save_path: Path) -> None:
+    with urllib.request.urlopen(url) as f:
+        file =
+    pass
