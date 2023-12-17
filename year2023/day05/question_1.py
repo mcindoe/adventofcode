75c82bfbf374a1767a7476921dd005383742cdd2
Conor McIndoe <conormcindoe1@gmail.com>
2023-12-17 08:44:54 +0000

    Add day 5 part 1 solution

--- year2023/day05/question_1.py
+++ year2023/day05/question_1.py
@@ -41,15 +41,8 @@ EXAMPLE = [
 class SourceToDestinationMap:
     def __init__(self, range_descriptions: list[str]):
         self.__range_descriptions = tuple(
-            (
-                int(destination_range_start),
-                int(source_range_start),
-                int(range_length),
-            )
+            tuple(int(x) for x in entry.strip().split(" "))
             for entry in range_descriptions
-            for destination_range_start, source_range_start, range_length in entry.strip().split(
-                " "
-            )
         )
 
     def __call__(self, x: int) -> int:
@@ -64,21 +57,64 @@ class SourceToDestinationMap:
         return x
 
 
-def get_seeds(puzzle_line: str) -> tuple[int]:
-    assert puzzle_line.startswith("seeds:")
-    return tuple(int(x) for x in puzzle_line.lstrip("sedes:").strip().split(" "))
+def get_seeds(puzzle_input: list[str]) -> tuple[int]:
+    first_line = puzzle_input[0]
+    assert first_line.startswith("seeds:")
+    return tuple(int(x) for x in first_line.lstrip("seeds:").strip().split(" "))
 
 
-def get_maps(puzzle_input) -> list[SourceToDestinationMap]:
+def get_maps(puzzle_input: list[str]) -> tuple[SourceToDestinationMap]:
     map_description_line_numbers = tuple(
         line_number for line_number, line in enumerate(puzzle_input) if "map:" in line
     )
-    for idx, line_number in enumerate(map_description_indices):
-        pass
 
-    pass
+    map_range_description_bounds = tuple(
+        (
+            map_description_line_numbers[idx] + 1,
+            map_description_line_numbers[idx + 1] - 1,
+        )
+        for idx in range(len(map_description_line_numbers) - 1)
+    ) + ((map_description_line_numbers[-1] + 1, len(puzzle_input)),)
+
+    return tuple(
+        SourceToDestinationMap(
+            puzzle_input[range_description_start:range_description_stop]
+        )
+        for range_description_start, range_description_stop in map_range_description_bounds
+    )
+
+
+def get_seed_locations(puzzle_input: list[str]) -> dict[int, int]:
+    seeds = get_seeds(puzzle_input)
+    source_to_destination_maps = get_maps(puzzle_input)
+
+    seed_destinations = {}
+    for seed in seeds:
+        location = seed
+        for source_to_destination_map in source_to_destination_maps:
+            location = source_to_destination_map(location)
+
+        seed_destinations[seed] = location
+
+    return seed_destinations
+
+
+def get_lowest_seed_location(puzzle_input: list[str]) -> int:
+    seed_locations = get_seed_locations(puzzle_input)
+    return min(seed_locations.values())
+
+
+def run_tests():
+    example_seed_locations = get_seed_locations(EXAMPLE)
+    expected_seed_locations = {79: 82, 14: 43, 55: 86, 13: 35}
+    assert example_seed_locations == expected_seed_locations
 
 
 if __name__ == "__main__":
+    run_tests()
+
     with open(YEAR_2023_DIR / "day05/data.txt", "r", encoding="utf-8") as fp:
         question_input = fp.readlines()
+
+    lowest_seed_location = get_lowest_seed_location(question_input)
+    print(f"Lowest seed location: {lowest_seed_location}")
